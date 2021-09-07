01014066a600db4069ef578a48f6bec3120932db
Lucas Santos <lusantosatl@gmail.com>
2021-09-07 10:17:21 -0400

    word-guessing functionality is complete

--- script.js
+++ script.js
@@ -7,7 +7,36 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
+const correctLetters = [];
+const wrongLetters = [];
+
+
+//show the hidden word
+function displayWord() {
+    //set the innerHTML of the wordEl to the selected word
+    wordEl.innerHTML = ` 
+        ${selectedWord
+            //split it into an array
+            .split('')
+            //map through said array
+            .map(letter => 
+                //check if the letter is included in that array. If it is, output the letter
+                // if it isnt, return an empty string
+                `<span class=letters>
+                    ${correctLetters.includes(letter) ? letter : ''}
+                </span>`).join('')} 
+  `;// turn it back into a string with .join()
+
+  const innerWord = wordEl.innerText.replace(/\n/g,'');
+  //console.log(wordEl.innerText, innerWord)
+  if(innerWord == selectedWord){
+      finalMessage.innerText =  'Congratulations! You won!';
+      popup.style.display = 'flex';
+  }
+}
+
+displayWord();
